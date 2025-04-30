c6a31e38ba64b868e57833be46c4e6779d328cda
Cstan0824 <149685655+Cstan0824@users.noreply.github.com>
2025-04-30 15:14:45 +0800

    Update functions.php
    Link S3 bucket assets to web app

--- wp-content/themes/blocksy-child/functions.php
+++ wp-content/themes/blocksy-child/functions.php
@@ -8,3 +8,18 @@ add_action( 'wp_enqueue_scripts', function () {
 	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 });
 
+function rewrite_asset_urls_to_s3($url) {
+    // Define your CDN or S3 base URL
+    $cdn_base = 'https://tarumt-grad-hub-bucket.s3.us-east-1.amazonaws.com'; // or your S3 public URL
+
+    // Only replace for wp-includes and wp-content assets
+    if (strpos($url, '/wp-includes/') !== false || strpos($url, '/wp-content/') !== false) {
+        $url = $cdn_base . parse_url($url, PHP_URL_PATH);
+    }
+
+    return $url;
+}
+
+add_filter('script_loader_src', 'rewrite_asset_urls_to_s3');
+add_filter('style_loader_src', 'rewrite_asset_urls_to_s3');
+
